0e208dbe91c0003c1935dc9be831e395005ff6a8
Knight, Eddie <iv.eddieknight@gmail.com>
2019-10-16 22:14:54 -0400

    Updated requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://https://github.com/eddie-knight/slimmyorm",
     packages=setuptools.find_packages(),
+    install_requires=[
+            'mysql-connector-python',
+        ],
     classifiers=[
+        'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
