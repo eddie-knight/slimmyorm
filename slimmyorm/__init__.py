9123fc5456abe8e80fe658a012ae25edded82d9c
Knight, Eddie <iv.eddieknight@gmail.com>
2019-10-16 22:30:57 -0400

    Fixed local imports

--- slimmyorm/__init__.py
+++ slimmyorm/__init__.py
@@ -1,3 +1,3 @@
-from base_data import BaseData
-from mysql_connection import ORM, MysqlConnection
-from select import Select
+from .base_data import BaseData
+from .mysql_connection import ORM, MysqlConnection
+from .select import Select
