785910af529e23eac1a965f165e6a8833bfb871d
Knight, Eddie <iv.eddieknight@gmail.com>
2019-10-16 21:36:57 -0400

    Restructured package for PyPi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slimmyorm",
+    version="0.0.1",
+    author="Eddie Knight",
+    description="Lightweight wrapper for mysql.connector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/eddie-knight/slimmyorm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
